dbbe1b55295beccdad08b596e661cc95e7a8d745
Harsh Gajera <103769647+theharshgajera@users.noreply.github.com>
2025-11-27 15:38:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from youtube_api import get_viral_thumbnails
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFound, VideoUnavailable
 import isodate
-from script_generate import generate_script_from_titleshorts
+from script_generate import generate_script_from_title
 from thumbnail_review import review_thumbnail
 import base64
 import googleapiclient.discovery
